54e6152f5448f8103d6f8317f6b39c177a3ac358
Tidjini <tidjini.messaoudi@gmail.com>
2022-11-13 18:19:05 +0100

    update service

--- service.py
+++ service.py
@@ -1,29 +1,56 @@
 import os
 import requests
-from helpers import formelize
+from helpers import formalize
 
 API = os.environ.get("PROCOM_API", "https://procom-tracker.herokuapp.com/api/")
 
 
-def update(product):
-    """Update in remote services
+def serialize(product: dict) -> tuple:
+    """Serialize product object
 
-    references must be lower case with no slashes
+    references must be lower case and no slashes
     """
-    reference = formelize(product["reference"])
+    reference = formalize(product["reference"])
 
-    json = {
+    data = {
         "qte_stock": product["qte_stock"],
         "value": product["value"],
-        "reference": product["reference"],
+        "reference": reference,
         "designation": product["designation"],
     }
-    files = {
-        "qte_picture": open(f"output/{reference}.qte.png", "rb"),
-        "value_picture": open(f"output/{reference}.mtn.png", "rb"),
-    }
-    # since the product (reference) exist in Remote API,
-    # otherwise make sure to have references saved in remote data
-    response = requests.put(f"{API}products/{reference}/", files=files, data=json)
 
-    print(response)
+    return reference, data
+
+
+def save(ref: str, data: dict):
+    """Save to remote host
+
+    If product reference exist then update with put request,
+    else create a new product object
+    """
+
+    url = f"{API}products/"
+
+    files = {
+        "qte_picture": open(f"output/{ref}.qte.png", "rb"),
+        "value_picture": open(f"output/{ref}.mtn.png", "rb"),
+    }
+
+    response = requests.put(f"{url}{ref}/", data=data, files=files)
+    if response.status_code == 200:
+        return response
+
+    response = requests.post(url, data=data)
+    if response.status_code == 201:
+        return response
+
+    error_message = "Save Exception, something went wrong in your remote server"
+    raise Exception(error_message, f"caused by: {response.json()}")
+
+
+def update(product):
+    ref, data = serialize(product)
+    try:
+        save(ref, data)
+    except Exception as e:
+        print("Exception due to:", e)
