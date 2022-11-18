52146edfb4a5c89dfd228fd9d8fde3d9fc6d7c3f
Tidjini <tidjini.messaoudi@gmail.com>
2022-11-18 08:21:32 +0100

    Set some constants

--- constants.py
+++ constants.py
@@ -1,19 +1,20 @@
-x_check, y_check = 118, 46
+x_etat_livraison, y_etat_livraison = 118, 46
+x_journal_encaissement, y_journal_encaissement = 93, 48
 x_logo, y_logo = 11, 11
 # track some spesific product reference
-PRODUCTS = (
-    ("0/1", "CUT BACK 0/1"),
-    ("BR", "BITUME ROUTIER"),
-    ("BRP", "BITUME 40/50 PORT"),
-    ("4/6", "Cut Back lourd 400/600"),
-    ("E65", "Emulsion 65 repandage"),
-    ("KERO", "KEROSENE"),
-)
 # PRODUCTS = (
 #     ("0/1", "CUT BACK 0/1"),
 #     ("BR", "BITUME ROUTIER"),
 #     ("BRP", "BITUME 40/50 PORT"),
+#     ("4/6", "Cut Back lourd 400/600"),
+#     ("E65", "Emulsion 65 repandage"),
+#     ("KERO", "KEROSENE"),
 # )
+PRODUCTS = (
+    ("0/1", "CUT BACK 0/1"),
+    ("BR", "BITUME ROUTIER"),
+    ("BRP", "BITUME 40/50 PORT"),
+)
 
 DATE_DEBUT_POSITION = 10, 95
 DATE_FIN_POSITION = 100, 95
@@ -21,7 +22,7 @@ REFERENCE_POSITION = 190, 95
 # date from 01/10/2019
 DATE_DEBUT = "01102019"
 REFRESH_TIME = 0
-NEXT_PRODUCT = 20
+NEXT_PRODUCT = 22
 QTE_REGION = (94, 760, 78, 15)
 MTN_REGION = (475, 775, 115, 16)
 BOTTOM_LINE = (1000, 1050)
