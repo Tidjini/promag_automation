aa747ba2ca29fd9462fa0bc4b706088ac13748c6
Tidjini <tidjini.messaoudi@gmail.com>
2022-11-18 11:20:49 +0100

    clean some stuff in delivery status

--- apps/procom/delivery_status.py
+++ apps/procom/delivery_status.py
@@ -1,8 +1,10 @@
 import time
+from _typeshed import Self
+
 from .window import Window
 
 
-class DelivryStatus(Window):
+class DeliveryStatus(Window):
     """Delivery Status Window
 
     perform actions:
@@ -13,12 +15,14 @@ class DelivryStatus(Window):
         - press the search button
     """
 
-    def perform_actions(self, start_date, end_date, reference, wait_time=20):
+    def perform_actions(self, start_date, end_date, reference, wait_time=20) -> None:
         """Perform Actions:
 
         Search with: Enter start date, end date with product ref, and press on search
         with sleep to wait for results (software performance 20s default)
         """
+
+        # if this window is not running go out
         if not self.is_running:
             return
 
@@ -34,14 +38,18 @@ class DelivryStatus(Window):
 
         time.sleep(wait_time)
 
-    def __enter__(self):
+    def __enter__(self) -> Self:
+        """use with keyword to perform actions in this object"""
         if self.is_running:
             return self
         try:
+            # click status button in main window menu
             self.click_asset(self.status)
             time.sleep(1)
+            # click in delivery status in displayed menu
             self.click_asset(self.delivery_status)
             time.sleep(2)
+            # after that delivery status is opened
             self.is_running = True
         except TypeError as e:
             self.is_running = False
@@ -49,24 +57,26 @@ class DelivryStatus(Window):
         return self
 
     def __exit__(self) -> None:
+        """exit the with keyword with trying to close the window"""
+
         if not self.is_running:
             return
+
         self.is_running = False
         try:
-            close_asset = self.close
-            self.click_asset(close_asset)
+            self.click_asset(self.close_asset)
             time.sleep(1)
         except TypeError as e:
             pass
 
 
-def checker(window: Window):
-    """Run this in seprate thread"""
-    window.checking()
-    window.double_check()
+# def checker(window: Window):
+#     """Run this in seprate thread"""
+#     window.checking()
+#     window.double_check()
 
 
-def service(window: Window):
-    with open(window) as win:
-        for n in ["products..."]:
-            win.perform_actions(...)
+# def service(window: Window):
+#     for n in ["products..."]:
+#         with open(window) as win:
+#             win.perform_actions(...)
