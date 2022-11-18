c2840308dfc7c6676ea42f7d71db6c226b8cb0c7
Tidjini <tidjini.messaoudi@gmail.com>
2022-11-18 08:23:14 +0100

    Add Services API

--- service.py
+++ service.py
@@ -1,56 +1,139 @@
 import os
 import requests
-from helpers import formalize
+from requests.exceptions import RequestException
+from helpers import formalize, raise_requests_exception
 
-API = os.environ.get("PROCOM_API", "https://procom-tracker.herokuapp.com/api/")
-
-
-def serialize(product: dict) -> tuple:
-    """Serialize product object
-
-    references must be lower case and no slashes
-    """
-    reference = formalize(product["reference"])
-
-    data = {
-        "qte_stock": product["qte_stock"],
-        "value": product["value"],
-        "reference": reference,
-        "designation": product["designation"],
-    }
-
-    return reference, data
+API = os.environ.get("PROCOM_API", None)
 
 
-def save(ref: str, data: dict):
-    """Save to remote host
+class ServiceAPI:
+    """Service Api, push data to remote server"""
 
-    If product reference exist then update with put request,
-    else create a new product object
-    """
+    @staticmethod
+    def post(url: str, data: dict, timeout: int = 15) -> tuple:
+        """Post data if not exist
 
-    url = f"{API}products/"
+        return True if created else False, along with the response object
+        """
+        try:
+            response = requests.post(url, data=data, timeout=timeout)
+            if response.status_code == 201:
+                return True, response
+        except RequestException as e:
+            print("POST Exception, due to:", e)
 
-    files = {
-        "qte_picture": open(f"output/{ref}.qte.png", "rb"),
-        "value_picture": open(f"output/{ref}.mtn.png", "rb"),
-    }
+        return False, response
 
-    response = requests.put(f"{url}{ref}/", data=data, files=files)
-    if response.status_code == 200:
-        return response
+    @staticmethod
+    def put(
+        url: str, reference: str, data: dict, files: dict = None, timeout: int = 15
+    ) -> tuple:
+        """Post data if not exist
 
-    response = requests.post(url, data=data)
-    if response.status_code == 201:
-        return response
+        return True if created else False, along with the response object
+        """
+        try:
+            response = requests.put(
+                f"{url}/{reference}/", data=data, files=files, timeout=timeout
+            )
+            if response == 200:
+                return True, response
+        except RequestException as e:
+            print("PUT Exception, due to:", e)
 
-    error_message = "Save Exception, something went wrong in your remote server"
-    raise Exception(error_message, f"caused by: {response.json()}")
+        return False, response
+
+    @staticmethod
+    def save(
+        url: str,
+        path: str,
+        reference: str,
+        data: dict,
+        files: dict = None,
+        timeout: int = 15,
+    ) -> tuple:
+        """Save data with both post and put
+
+        requests issue with post data and file request
+        - first we put data
+        - if there is no data we create one, then we push the files if exist
+        """
+
+        url_path = f"{url}/{path}/"
+
+        # try to put data
+        saved, response = ServiceAPI.put(
+            url=url_path, reference=reference, data=data, files=files, timeout=timeout
+        )
+
+        if saved:
+            return response
+
+        # if not saved post just data
+        saved, response = ServiceAPI.post(url=url_path, data=data, timeout=timeout)
+        if saved:
+            if files:
+                # if saved send files with data (issue with request post with files)
+                saved, response = ServiceAPI.put(
+                    url=url_path,
+                    reference=reference,
+                    data=data,
+                    files=files,
+                    timeout=timeout,
+                )
+            return response
+
+        raise_requests_exception(response)
 
 
-def update(product):
-    ref, data = serialize(product)
-    try:
-        save(ref, data)
-    except Exception as e:
-        print("Exception due to:", e)
+class ProductServiceAPI:
+    """Specific APi for product"""
+
+    @staticmethod
+    def format(product: dict) -> tuple:
+        """Format Product instantance
+
+        return reference to correct forme, ex: 1/0 -> 1_0
+        """
+        reference = formalize(product["reference"])
+        product["reference"] = reference
+        return reference, product
+
+    @staticmethod
+    def prepare_file(filename: str, path: str = "output"):
+        """Get file with binary read by filename"""
+        try:
+            return open(f"{path}/{filename}", "rb")
+        except FileNotFoundError as e:
+            print("Prepare File Exception, due to:", e)
+            return None
+
+    @staticmethod
+    def prepare_files(files: dict, path: str = "output"):
+        """Prepare a list of files"""
+        result = {}
+        for attribute, filename in files.items():
+            file = ProductServiceAPI.prepare_file(filename, path)
+            if file is None:
+                continue
+            result[attribute] = file
+
+        return result
+
+    @staticmethod
+    def save(path: str, instance: dict, filenames: dict = None) -> None:
+        """Specific save methd for product instance"""
+
+        if not API:
+            raise Exception("Specify your API Url in your Environment")
+        try:
+            # get formated data
+            reference, product = ProductServiceAPI.format(instance)
+            # set files
+            files = ProductServiceAPI.prepare_files(filenames)
+            # save data, files to remote service
+            ServiceAPI.save(
+                url=API, path=path, reference=reference, data=product, files=files
+            )
+        except Exception as e:
+            print("<ProductServiceApi.Save>, Exception due to:", e)
